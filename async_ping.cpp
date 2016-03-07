85a2c4394a5d2228bdfad3e3d3bd38e8fe599440
pixley <tylerpixley@gmail.com>
2016-03-07 10:40:24 -0600

    Going off raw example, need to see if 'which' has a consistent location

--- async_ping.cpp
+++ async_ping.cpp
@@ -19,6 +19,98 @@ struct thread_info
 	thread_info(bool& comp, std::string& buffer) : complete(comp), buf(buffer) {};
 };
 
+std::string which_ping()
+{
+	int exit_code;
+	int cout_pipe[2];
+	int cerr_pipe[2];
+	posix_spawn_file_actions_t action;
+
+	if(pipe(cout_pipe) || pipe(cerr_pipe))
+    {
+    	std::cout << "pipe returned an error." << std::endl;
+    	return "";
+    }
+
+	posix_spawn_file_actions_init(&action);
+	posix_spawn_file_actions_addclose(&action, cout_pipe[0]);
+	posix_spawn_file_actions_addclose(&action, cerr_pipe[0]);
+	posix_spawn_file_actions_adddup2(&action, cout_pipe[1], 1);
+	posix_spawn_file_actions_adddup2(&action, cerr_pipe[1], 2);
+
+	posix_spawn_file_actions_addclose(&action, cout_pipe[1]);
+	posix_spawn_file_actions_addclose(&action, cerr_pipe[1]);
+
+	std::string argsmem[] = {"/usr/bin/which", "ping"};
+	char* args[] = {&argsmem[0][0], &argsmem[1][0], nullptr};
+
+	pid_t pid;
+	if(posix_spawnp(&pid, args[0], &action, NULL, &args[0], NULL) != 0)
+	{
+    	std::cout << "posix_spawnp failed with error: " << strerror(errno) << "\n";
+    	exit(-2);
+    }
+
+    if (waitpid(pid,&exit_code,WNOHANG) == -1)
+    {
+    	std::cout << "Child process was invalid." << std::endl;
+    	return "";
+    }
+
+    // close child-side of pipes
+    close(cout_pipe[1]);
+    close(cerr_pipe[1]);
+
+    std::cout << "Started 'which' process, PID is " << pid << std::endl; 
+
+    // Wait for execution to finish
+    struct timespec sleep_time;
+    sleep_time.tv_nsec = 16000000;
+	nanosleep(&sleep_time, NULL);
+
+    std::string buf(64,' ');
+
+    struct timeval timeout = {5, 0};
+
+    fd_set read_set;
+    memset(&read_set, 0, sizeof(read_set));
+    FD_SET(cout_pipe[0], &read_set);
+    FD_SET(cerr_pipe[0], &read_set);
+
+    int larger_fd = (cout_pipe[0] > cerr_pipe[0]) ? cout_pipe[0] : cerr_pipe[0];
+
+    int rc = select(larger_fd + 1, &read_set, NULL, NULL, &timeout);
+    //thread blocks until either packet is received or the timeout goes through
+    if (rc == 0)
+    {
+        std::cout << "Timed out." << std::endl;
+        return "";
+    }
+
+	int bytes_read = read(cerr_pipe[0], &buf[0], buf.length());
+	if (bytes_read > 0)
+	{
+		std::cout << "Got error message: " << buf.substr(0, bytes_read) << std::endl;
+		return "";
+	}
+
+    bytes_read = read(cout_pipe[0], &buf[0], buf.length());
+	if (bytes_read > 0)
+	{
+		std::cout << "Read in " << bytes_read << " bytes from cout_pipe." << std::endl;
+	}
+	else
+	{
+		std::cout << "Read nothing from cout_pipe." << std::endl;
+	}
+
+	waitpid(pid,&exit_code,0);
+	
+	posix_spawn_file_actions_destroy(&action);
+
+    return buf.substr(0,bytes_read);
+}
+
 void* read_pipe(void* info)
 {
 	struct thread_info* t_info = (struct thread_info*)info;
@@ -34,6 +126,16 @@ void* read_pipe(void* info)
 		exit(-1);
 	}
 
+	std::string ping_path = which_ping();
+
+	if (ping_path.length() == 0)
+	{
+		std::cout << "Was unable to find ping executable." << std::endl;
+		exit(-7);
+	}
+
+	std::cout << "Ping path: " << ping_path << std::endl;
+
 	posix_spawn_file_actions_init(&action);
 	posix_spawn_file_actions_addclose(&action, cout_pipe[0]);
 	posix_spawn_file_actions_addclose(&action, cerr_pipe[0]);
@@ -43,8 +145,7 @@ void* read_pipe(void* info)
 	posix_spawn_file_actions_addclose(&action, cout_pipe[1]);
 	posix_spawn_file_actions_addclose(&action, cerr_pipe[1]);
 
-	//char command[] = "/sbin/ping -c 1 8.8.4.4";
-	std::string argsmem[] = {"/sbin/ping", "-c", "1", "8.8.4.4"};
+	std::string argsmem[] = {ping_path, "-c", "1", "8.8.4.4"};
 	char* args[] = {&argsmem[0][0], &argsmem[1][0], &argsmem[2][0], &argsmem[3][0], nullptr};
 
 	pid_t pid;
@@ -60,17 +161,14 @@ void* read_pipe(void* info)
     close(cout_pipe[1]);
     close(cerr_pipe[1]);
 
-    struct timespec sleep_time;
-    sleep_time.tv_nsec = 16000000;
-
 	bool& complete = t_info->complete;
 	std::string& parent_buf = t_info->buf;
 
-	std::string buf(1024,' ');
+	std::string buf(512,' ');
 	std::stringstream bufstream;
 
 	std::vector<pollfd> plist = { {cout_pipe[0],POLLIN}, {cerr_pipe[0],POLLIN} };
-	for ( int rval; (rval=poll(&plist[0],plist.size(),/*timeout*/5000))>0; ) {
+	for ( int rval; (rval=poll(&plist[0],plist.size(),5000))>0; ) {
     	if ( plist[0].revents&POLLIN) {
     		size_t bytes_read = read(cout_pipe[0], &buf[0], buf.length());
     		if (bytes_read > 0)
@@ -96,7 +194,6 @@ void* read_pipe(void* info)
     	{
     		break;
     	}
-    	nanosleep(&sleep_time, NULL);
 	}
 
 	//std::cout << "Done reading." << std::endl;
---
+++ test_spawn_pipe.cpp
@@ -0,0 +1,100 @@
+/*BINFMTCXX: -std=c++11 -Wall -Werror
+*/
+
+#include <spawn.h> // see manpages-posix-dev
+#include <poll.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+  int exit_code;
+  int cout_pipe[2];
+  int cerr_pipe[2];
+  posix_spawn_file_actions_t action;
+
+  if(pipe(cout_pipe) || pipe(cerr_pipe))
+    cout << "pipe returned an error.\n";
+
+  posix_spawn_file_actions_init(&action);
+  posix_spawn_file_actions_addclose(&action, cout_pipe[0]);
+  posix_spawn_file_actions_addclose(&action, cerr_pipe[0]);
+  posix_spawn_file_actions_adddup2(&action, cout_pipe[1], 1);
+  posix_spawn_file_actions_adddup2(&action, cerr_pipe[1], 2);
+
+  posix_spawn_file_actions_addclose(&action, cout_pipe[1]);
+  posix_spawn_file_actions_addclose(&action, cerr_pipe[1]);
+
+  string command = "/usr/bin/which ping";
+  // string command = "pgmcrater -width 64 -height 9 |pgmtopbm |pnmtoplainpnm";
+  string argsmem[] = {"bash","-l","-c"}; // allows non-const access to literals
+  char * args[] = {&argsmem[0][0],&argsmem[1][0],&argsmem[2][0],&command[0],nullptr};
+  //char * args[] = {&command[0],nullptr};
+
+  pid_t pid;
+  if(posix_spawnp(&pid, args[0], &action, NULL, &args[0], NULL) != 0)
+    cout << "posix_spawnp failed with error: " << strerror(errno) << "\n";
+
+  close(cout_pipe[1]), close(cerr_pipe[1]); // close child-side of pipes
+
+  // Read from pipes
+  string buffer(1024,' ');
+  /*
+  std::vector<pollfd> plist = { {cout_pipe[0],POLLIN}, {cerr_pipe[0],POLLIN} };
+  for ( int rval; (rval=poll(&plist[0],plist.size(),-1))>0; ) {
+    if ( plist[0].revents&POLLIN) {
+      int bytes_read = read(cout_pipe[0], &buffer[0], buffer.length());
+      cout << "read " << bytes_read << " bytes from stdout.\n";
+      cout << buffer.substr(0, static_cast<size_t>(bytes_read)) << "\n";
+    }
+    else if ( plist[1].revents&POLLIN ) {
+      int bytes_read = read(cerr_pipe[0], &buffer[0], buffer.length());
+      cout << "read " << bytes_read << " bytes from stderr.\n";
+      cout << buffer.substr(0, static_cast<size_t>(bytes_read)) << "\n";
+    }
+    else break; // nothing left to read
+  }
+  */
+
+  struct timeval timeout = {5, 0};
+
+  fd_set read_set;
+  memset(&read_set, 0, sizeof(read_set));
+  FD_SET(cout_pipe[0], &read_set);
+  FD_SET(cerr_pipe[0], &read_set);
+
+  int larger_fd = (cout_pipe[0] > cerr_pipe[0]) ? cout_pipe[0] : cerr_pipe[0];
+
+  int rc = select(larger_fd + 1, &read_set, NULL, NULL, &timeout);
+  //thread blocks until either packet is received or the timeout goes through
+  if (rc == 0)
+  {
+      std::cout << "Timed out." << std::endl;
+      return 1;
+  }
+
+  int bytes_read = read(cerr_pipe[0], &buffer[0], buffer.length());
+  if (bytes_read > 0)
+  {
+    std::cout << "Got error message: " << buffer.substr(0, bytes_read) << std::endl;
+  }
+
+    bytes_read = read(cout_pipe[0], &buffer[0], buffer.length());
+  if (bytes_read > 0)
+  {
+    //std::cout << "Read in " << bytes_read << " bytes from cout_pipe." << std::endl;
+    std::cout << buffer.substr(0, bytes_read) << std::endl;
+  }
+
+  waitpid(pid,&exit_code,0);
+  cout << "exit code: " << exit_code << "\n";
+
+  posix_spawn_file_actions_destroy(&action);
+}
